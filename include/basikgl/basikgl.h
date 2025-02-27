4693a023cd0b4dafc5e81d29980af191320e1395
Arnav Deshpande <arnavdeshpande222@gmail.com>
2025-02-28 03:25:01 +0530

    Alterted public api header to use relative includes.

--- include/basikgl/basikgl.h
+++ include/basikgl/basikgl.h
@@ -5,52 +5,52 @@
  */
 
 /// @dir core
-#include <core/core.h>
-#include <core/error_handler.h>
-#include <core/logger.h>
-#include <core/convert_values.h>
+#include "../../src/core/core.h"
+#include "../../src/core/error_handler.h"
+#include "../../src/core/logger.h"
+#include "../../src/core/convert_values.h"
 
 /// @dir time
-#include <time/timespan.h>
-#include <time/clock.h>
+#include "../../src/time/timespan.h"
+#include "../../src/time/clock.h"
 
 /// @dir context
-#include <context/asset_manager.h>
-#include <context/context_properties.h>
-#include <context/render_context.h>
-#include <context/context_manager.h>
+#include "../../src/context/asset_manager.h"
+#include "../../src/context/context_properties.h"
+#include "../../src/context/render_context.h"
+#include "../../src/context/context_manager.h"
 
 /// @dir window
-#include <window/window_attributes.h>
-#include <window/window_properties.h>
-#include <window/window_callbacks.h>
-#include <window/window.h>
+#include "../../src/window/window_attributes.h"
+#include "../../src/window/window_properties.h"
+#include "../../src/window/window_callbacks.h"
+#include "../../src/window/window.h"
 
 /// @dir gfx
-#include <gfx/vertex.h>
-#include <gfx/vertexbuffer.h>
-#include <gfx/indexbuffer.h>
-#include <gfx/vertexarray.h>
-#include <gfx/shader.h>
+#include "../../src/gfx/vertex.h"
+#include "../../src/gfx/vertexbuffer.h"
+#include "../../src/gfx/indexbuffer.h"
+#include "../../src/gfx/vertexarray.h"
+#include "../../src/gfx/shader.h"
 
 /// @dir sprite
-#include <sprite/sprite.h>
+#include "../../src/sprite/sprite.h"
 
 /// @dir gfx/texture
-#include <gfx/texture/texture.h>
-#include <gfx/texture/texture2d.h>
+#include "../../src/gfx/texture/texture.h"
+#include "../../src/gfx/texture/texture2d.h"
 
 /// @dir input
-#include <input/keyinput.h>
-#include <input/mouseinput.h>
-#include <input/cursor.h>
+#include "../../src/input/keyinput.h"
+#include "../../src/input/mouseinput.h"
+#include "../../src/input/cursor.h"
 
 /// @dir camera
-#include <camera/camera.h>
-#include <camera/gui_camera.h>
-#include <camera/perspective_camera.h>
-#include <camera/player_camera.h>
+#include "../../src/camera/camera.h"
+#include "../../src/camera/gui_camera.h"
+#include "../../src/camera/perspective_camera.h"
+#include "../../src/camera/player_camera.h"
 
 /// @dir utils
-#include <utils/utils.h>
-#include <utils/uuid_generator.h>
+#include "../../src/utils/utils.h"
+#include "../../src/utils/uuid_generator.h"
